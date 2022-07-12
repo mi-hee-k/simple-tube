f74f7f058ba3361cfa6a0a96d29a5cd287aa4719
mi-hee-k <kim323mh@gmail.com>
2022-07-12 21:16:36 +0900

    axios2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Youtube from "./service/youtube";
+import axios from "axios";
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3/",
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+const youtube = new Youtube(httpClient);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App youtube={youtube} />);
