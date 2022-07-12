0085d55ba79a2057ea0a7dd58c917b212ae9850a
mi-hee-k <kim323mh@gmail.com>
2022-07-12 18:03:02 +0900

    헤더와 검색 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import Header from "./Header";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCqIWbzTYxprOXEE9iJbrA2TGAH4vYtI3U`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -23,7 +40,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Header onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,36 @@
-const Header = () => {
+import { useRef } from "react";
+
+const Header = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+  const onClick = (e) => {
+    handleSearch();
+  };
+  const onKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
-    <div className="header">
-      <h1>Youtube</h1>
-      <form>
-        <input />
-        <button></button>
-      </form>
-    </div>
+    <header>
+      <div className="logo">
+        <img src="./images/logo.png" alt="logo" />
+        <h1>Youtube</h1>
+      </div>
+      <input
+        ref={inputRef}
+        type="search"
+        placeholder="검색어를 입력하세요"
+        onKeyDown={onKeyDown}
+      />
+      <button type="submit">
+        <img src="./images/search.png" alt="search" onClick={onClick} />
+      </button>
+    </header>
   );
 };
 
